d685914e2d3eb775a9f9580fa7c83c1069ef6ca8
aferna08 <abfernandez08@gmail.com>
2025-01-15 09:41:46 -0500

    Initial commit with project files

---
+++ scripts/01_analysis.R
@@ -0,0 +1,104 @@
+pacman::p_load(tidyverse, here, fixest, modelsummary)
+
+# Load Data
+iv_data <- read_csv(here("data", "iv_data.csv"))
+
+#### Run IV Models ####
+# Clay
+one_iv <- feols(
+  I(lead_h_change * 100) ~ factor(decade)*scale(pct_ba) + scale(log(decade_pop)) + 
+    scale(pct_black) + I(scale(pct_black) ^ 2) + scale(pct_hispanic) + scale(pct_asian) + scale(pct_fb) +
+    scale(pct_elderly) + scale(pct_poverty) + scale(pct_military) + scale(muni_pcap) +
+    scale(pct_government) + scale(pct_manufacturing) + scale(gdp_pcap) + scale(pct_largehh) + scale(pct_pop_growth) +
+    factor(region) | scale(I(units_pcap * 1000)) ~ scale(clay),
+  data = iv_data, cluster = "cbsa_code"
+)
+
+# Two Characteristics
+two_iv <- feols(
+  I(lead_h_change * 100) ~ factor(decade)*scale(pct_ba) + scale(log(decade_pop)) + 
+    scale(pct_black) + I(scale(pct_black) ^ 2) + scale(pct_hispanic) + scale(pct_asian) + scale(pct_fb) +
+    scale(pct_elderly) + scale(pct_poverty) + scale(pct_military) + scale(muni_pcap) +
+    scale(pct_government) + scale(pct_manufacturing) + scale(gdp_pcap) + scale(pct_largehh) + scale(pct_pop_growth) +
+    factor(region) | scale(I(units_pcap * 1000)) ~ scale(clay) + scale(mean_ksat),
+  data = iv_data, cluster = "cbsa_code"
+)
+
+# All Characteristics
+all_iv <- feols(
+  I(lead_h_change * 100) ~ factor(decade)*scale(pct_ba) + scale(log(decade_pop)) + 
+    scale(pct_black) + I(scale(pct_black) ^ 2) + scale(pct_hispanic) + scale(pct_asian) + scale(pct_fb) +
+    scale(pct_elderly) + scale(pct_poverty) + scale(pct_military) + scale(muni_pcap) +
+    scale(pct_government) + scale(pct_manufacturing) + scale(gdp_pcap) + scale(pct_largehh) + scale(pct_pop_growth) +
+    factor(region) | scale(I(units_pcap * 1000)) ~ scale(clay) + scale(mean_ksat) + 
+    scale(rock_fragments) + scale(restrictive_depth) + scale(bulk_density),
+  data = iv_data, cluster = "cbsa_code"
+)
+
+#### Table Formatting ####
+
+# create rows of sargan and F-tests to add to model summary
+rows <- tribble(
+  ~term, ~model1, ~model2, ~model3,
+  "First Stage F-Statistic", 
+  fitstat(one_iv, "ivf", simplify = TRUE)[[1]],
+  fitstat(two_iv, "ivf", simplify = TRUE)[[1]],
+  fitstat(all_iv, "ivf", simplify = TRUE)[[1]],
+  "Sargan Test", 
+  NA,
+  fitstat(two_iv, "sargan", simplify = TRUE)[[1]],
+  fitstat(all_iv, "sargan", simplify = TRUE)[[1]],
+  "Sargan P-Value", 
+  NA, 
+  fitstat(two_iv, "sargan", simplify = TRUE)[[2]],
+  fitstat(all_iv, "sargan", simplify = TRUE)[[2]]
+)
+
+# Create coefficient map to clean variable names
+cm <- c(
+  "(Intercept)" = "Intercept",
+  "factor(decade)2000" = "2000s",
+  "factor(decade)2010" = "2010s",
+  "factor(year)2000" = "2000s",
+  "factor(year)2010" = "2010s",
+  "scale(I(units_pcap * 1000))" = "Units per 1,000 People",
+  "fit_scale(I(units_pcap * 1000))" = "Units per 1,000 People",
+  "scale(pct_ba)" = "Bachelor's Degree (%)",
+  "factor(decade)2000:scale(pct_ba)" = "2000s x Bachelor's %",
+  "factor(decade)2010:scale(pct_ba)" = "2010s x Bachelor's %",
+  "factor(year)2000:scale(pct_ba)" = "2000s x Bachelor's %",
+  "factor(year)2010:scale(pct_ba)" = "2010s x Bachelor's %",
+  "scale(pct_black)" = "Black (%)",
+  "I(scale(pct_black)^2)" = "Black (%) Squared",
+  "scale(pct_hispanic)" = "Hispanic (%)",
+  "scale(pct_asian)" = "Asian (%)",
+  "scale(pct_fb)" = "Foreign-Born (%)",
+  "scale(pct_elderly)" = "Elderly (%)",
+  "scale(pct_poverty)" = "Poverty (%)",
+  "scale(gdp_pcap)" = "GDP per Capita",
+  "scale(pct_manufacturing)" = "Manufacturing (%)",
+  "scale(pct_government)" = "Government (%)",
+  "scale(pct_military)" = "Military (%)",
+  "scale(pct_largehh)" = "Large Household (%)",
+  "scale(muni_pcap)" = "Municipal Fragmentation",
+  "scale(pct_pop_growth)" = "Population Growth",
+  "scale(log(decade_pop))" = "Population (log)",
+  "scale(log(pop))" = "Population (log)",
+  "factor(region)Northeast" = "Northeast",
+  "factor(region)South" = "South",
+  "factor(region)West" = "West",
+  "scale(clay)" = "Clay (%)",
+  "scale(mean_ksat)" = "Hydraulic Conductivity",
+  "scale(rock_fragments)" = "Rock Fragments (%)",
+  "scale(restrictive_depth)" = "Restrictive Depth (cm)",
+  "scale(bulk_density)" = "Bulk Density"
+)
+
+#### Output Table ####
+
+iv_table <- modelsummary(list("Clay Instrument" = one_iv, "Clay & Hydraulic \nConductivity" = two_iv, 
+                  "All Soil \nCharacteristics" = all_iv), 
+             stars = TRUE,
+             coef_map = cm,
+             gof_omit = "R2(?! Adj)|AIC|BIC|RMSE|Std.Errors",
+             add_rows = rows)
---
+++ scripts/02_seg_change_map.R
@@ -0,0 +1,79 @@
+pacman::p_load(ggiraph, tidyverse, patchwork)
+
+cbsa_wide <- read_csv(here("data", "cbsa_wide.csv"))
+
+cbsa_shp <- tigris::core_based_statistical_areas(year = 2013, cb = TRUE, progress_bar = FALSE) %>% 
+  tigris::shift_geometry() %>% 
+  janitor::clean_names() %>% 
+  mutate(cbsa_code = as.integer(cbsafp)) %>% 
+  inner_join(cbsa_wide, by = "cbsa_code")
+
+states_shp <- tigris::states(cb = TRUE, progress_bar = FALSE) %>% 
+  janitor::clean_names() %>% 
+  filter(as.integer(statefp) < 60 & !stusps %in% c("AK", "HI")) %>% 
+  sf::st_transform(sf::st_crs(cbsa_shp))
+
+map_interactive <- ggplot(data = cbsa_shp) + 
+  geom_sf(data = states_shp, fill = "white") +
+  geom_sf_interactive(
+    aes(
+      fill = h_change * 100,
+      data_id = cbsa_code,
+      tooltip = paste0(
+        "<strong>", cbsa_title, "</strong><br>",
+        "Units per 1,000 People: ", round(units_pcap * 1000, 2), "<br>",
+        "Segregation Change: ", round(h_change * 100, 1)
+        )
+      )) +
+  theme_void(base_size = 10) + 
+  scale_fill_viridis_c() + 
+  labs(
+    fill = ""
+  ) + 
+  theme(
+    legend.position = "bottom"
+  )
+
+scatter_interactive <- 
+  ggplot(data = cbsa_wide, 
+         aes(
+           x = 100 * h_change, y = 1000 * units_pcap,
+           data_id = cbsa_code,
+           tooltip = paste0(
+             "<strong>", cbsa_title, "</strong><br>",
+             "Units per 1,000 People: ", round(units_pcap * 1000, 2), "<br>",
+             "Segregation Change: ", round(h_change * 100, 1)
+           )
+         )) + 
+  geom_point_interactive(color = "#33638DFF") + 
+  theme_minimal() + 
+  labs(
+    x = "Segregation Change",
+    y = "Units per 1,000 People"
+  )
+
+map_options <- list(
+  opts_tooltip(opacity = .9,
+               delay_mouseout = 850,
+               delay_mouseover = 650,
+               css = "background-color: #99cadb; color: black;
+                      padding: 5px; border-radius: 5px; border-style: solid;
+                      border-color: black; border-width: 1.5px;
+                      font-size: 18px; font-family: Arial"),
+  opts_hover(),
+  opts_hover_inv(css = "opacity: .25;")
+)
+
+girafe_plot <- girafe(
+  ggobj = map_interactive + scatter_interactive,
+  options = map_options,
+  width_svg = 8,
+  height_svg = 3
+)
+
+htmlwidgets::saveWidget(
+  widget = girafe_plot,
+  file = here("output", "interactive_map.html"),
+  selfcontained = TRUE  # Embed all assets in a single HTML file
+)
+
